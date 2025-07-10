c0dbec6e7f10e9f3f2e461a11a2b68ef0536541e
Sachin <sachin.kharabanda@aol.com>
2025-07-10 11:19:39 +0100

    Completed algorithm

--- Smallest in array/Program.cs
+++ Smallest in array/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SmallestInArray(int[] x) {
+
+    int smallest = x[0];
+    for (int i = 1; i < x.Length; i++) {
+        if (x[i] < smallest)
+        {
+            smallest = x[i];
+        }
+    }
+    
+    return smallest;
+}
+
+int[] a = {7, 12, 9, 11, 3};
+Console.WriteLine(SmallestInArray(a));
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
